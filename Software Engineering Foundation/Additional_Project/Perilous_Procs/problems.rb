dd25f0c7d7a689212f199139d1303fce74376b56
BirukAbraham123 <biruk_abraham@yahoo.com>
2020-09-05 07:18:24 -0700

    Perilous_Procs questions from additional project is completed

---
+++ Software Engineering Foundation/Additional_Project/Perilous_Procs/problems.rb
@@ -0,0 +1,447 @@
+# Phase 1: Problems that are pleasant
+
+def some?(arr, &prc)
+  arr.each do |ele|
+    return true if prc.call(ele)
+  end
+  return false
+end
+
+p some?([3, 1, 11, 5]) { |n| n.even? }                                # false
+p some?([3, 4, 11, 5]) { |n| n.even? }                                # true
+p some?([8, 2]) { |n| n.even? }                                       # true
+p some?(['squash', 'corn', 'kale', 'carrot']) { |str| str[0] == 'p' } # false
+p some?(['squash', 'corn', 'kale', 'potato']) { |str| str[0] == 'p' } # true
+p some?(['parsnip', 'lettuce', 'pea']) { |str| str[0] == 'p' }        # true
+
+def exactly?(arr, n, &prc)
+  exact = 0
+  arr.each do |ele|
+    exact += 1 if prc.call(ele)
+  end
+  return exact == n
+end
+
+p exactly?(['A', 'b', 'C'], 2) { |el| el == el.upcase }         # true
+p exactly?(['A', 'B', 'C'], 2) { |el| el == el.upcase }         # false
+p exactly?(['A', 'B', 'C'], 3) { |el| el == el.upcase }         # true
+p exactly?(['cat', 'DOG', 'bird'], 1) { |el| el == el.upcase }  # true
+p exactly?(['cat', 'DOG', 'bird'], 0) { |el| el == el.upcase }  # false
+p exactly?(['cat', 'dog', 'bird'], 0) { |el| el == el.upcase }  # true
+p exactly?([4, 5], 3) { |n| n > 0 }                             # false
+p exactly?([4, 5, 2], 3) { |n| n > 0 }                          # true
+p exactly?([42, -9, 7, -3, -6], 2) { |n| n > 0 }                # true
+
+def filter_out(arr, &prc)
+  new_arr = []
+  arr.each do |ele|
+    new_arr << ele if !prc.call(ele)
+  end
+  return new_arr
+end
+
+p filter_out([10, 6, 3, 2, 5 ]) { |x| x.odd? }      # [10, 6, 2]
+p filter_out([1, 7, 3, 5 ]) { |x| x.odd? }          # []
+p filter_out([10, 6, 3, 2, 5 ]) { |x| x.even? }     # [3, 5]
+p filter_out([1, 7, 3, 5 ]) { |x| x.even? }         # [1, 7, 3, 5]
+
+def at_least?(arr, num, &prc)
+  count = 0
+  arr.each do |ele|
+    count += 1 if prc.call(ele)
+  end
+  return count >= num
+end
+
+p at_least?(['sad', 'quick', 'timid', 'final'], 2) { |s| s.end_with?('ly') }
+# false
+p at_least?(['sad', 'quickly', 'timid', 'final'], 2) { |s| s.end_with?('ly') }
+# false
+p at_least?(['sad', 'quickly', 'timidly', 'final'], 2) { |s| s.end_with?('ly') }
+# true
+p at_least?(['sad', 'quickly', 'timidly', 'finally'], 2) { |s| s.end_with?('ly') }
+# true
+p at_least?(['sad', 'quickly', 'timid', 'final'], 1) { |s| s.end_with?('ly') }
+# true
+p at_least?(['sad', 'quick', 'timid', 'final'], 1) { |s| s.end_with?('ly') }
+# false
+p at_least?([false, false, false], 3) { |bool| bool }
+# false
+p at_least?([false, true, true], 3) { |bool| bool }
+# false
+p at_least?([true, true, true], 3) { |bool| bool }
+# true
+p at_least?([true, true, true, true], 3) { |bool| bool }
+# true
+
+def every?(arr, &prc)
+  arr.each do |ele|
+    return false if !prc.call(ele)
+  end
+  return true
+end
+
+p every?([3, 1, 11, 5]) { |n| n.even? }                                 # false
+p every?([2, 4, 4, 8]) { |n| n.even? }                                  # true
+p every?([8, 2]) { |n| n.even? }                                        # true
+p every?(['squash', 'corn', 'kale', 'carrot']) { |str| str[0] == 'p' }  # false
+p every?(['squash', 'pea', 'kale', 'potato']) { |str| str[0] == 'p' }   # false
+p every?(['parsnip', 'potato', 'pea']) { |str| str[0] == 'p' }          # true
+
+def at_most?(arr, num, &prc)
+  count = 0
+  arr.each do |ele|
+    count += 1 if prc.call(ele)
+  end
+  return count <= num
+end
+
+p at_most?([-4, 100, -3], 1) { |el| el > 0 }                         # true
+p at_most?([-4, -100, -3], 1) { |el| el > 0 }                        # true
+p at_most?([4, 100, -3], 1) { |el| el > 0 }                          # false
+p at_most?([4, 100, 3], 1) { |el| el > 0 }                           # false
+p at_most?(['r', 'q', 'e', 'z'], 2) { |el| 'aeiou'.include?(el) }    # true
+p at_most?(['r', 'i', 'e', 'z'], 2) { |el| 'aeiou'.include?(el) }    # true
+p at_most?(['r', 'i', 'e', 'o'], 2) { |el| 'aeiou'.include?(el) }    # false
+
+def first_index(arr, &prc)
+  arr.each_with_index do |ele, idx|
+    return idx if prc.call(ele)
+  end
+  return nil
+end
+
+p first_index(['bit', 'cat', 'byte', 'below']) { |el| el.length > 3 }           # 2
+p first_index(['bitten', 'bit', 'cat', 'byte', 'below']) { |el| el.length > 3 } # 0
+p first_index(['bitten', 'bit', 'cat', 'byte', 'below']) { |el| el.length > 6 } # nil
+p first_index(['bit', 'cat', 'byte', 'below']) { |el| el[0] == 'b' }            # 0
+p first_index(['bit', 'cat', 'byte', 'below']) { |el| el.include?('a') }        # 1
+p first_index(['bit', 'cat', 'byte', 'below']) { |el| el[0] == 't' }            # nil
+
+# Phase 2: The proc thickens
+
+def xnor_select(arr, prc_one, prc_two)
+  new_arr = []
+  
+  arr.each do |ele|
+    if (prc_one.call(ele) && prc_two.call(ele)) || (!prc_one.call(ele) && !prc_two.call(ele))
+      new_arr << ele
+    end
+  end
+
+  return new_arr
+end
+
+is_even = Proc.new { |n| n % 2 == 0 }
+is_odd = Proc.new { |n| n % 2 != 0 }
+is_positive = Proc.new { |n| n > 0 }
+p xnor_select([8, 3, -4, -5], is_even, is_positive)         # [8, -5]
+p xnor_select([-7, -13, 12, 5, -10], is_even, is_positive)  # [-7, -13, 12]
+p xnor_select([-7, -13, 12, 5, -10], is_odd, is_positive)   # [5, -10]
+
+# require "byebug"
+
+def filter_out!(arr, &prc)
+  # debugger
+
+  arr.each_with_index do |ele, idx|
+    if prc.call(ele)
+      arr[idx] = nil
+    end
+  end
+  return arr.delete(nil)
+end
+
+arr_1 = [10, 6, 3, 2, 5 ]
+filter_out!(arr_1) { |x| x.odd? }
+p arr_1     # [10, 6, 2]
+
+arr_2 = [1, 7, 3, 5 ]
+filter_out!(arr_2) { |x| x.odd? }
+p arr_2     # []
+
+arr_3 = [10, 6, 3, 2, 5 ]
+filter_out!(arr_3) { |x| x.even? }
+p arr_3     # [3, 5]
+
+arr_4 = [1, 7, 3, 5 ]
+filter_out!([1, 7, 3, 5 ]) { |x| x.even? }
+p arr_4 # [1, 7, 3, 5]
+
+def multi_map(arr, n = 1, &prc)
+  (0...n).each do |i|
+    arr.each_with_index do |ele, idx|
+      arr[idx] = prc.call(ele)
+    end
+  end
+  return arr
+end
+
+p multi_map(['pretty', 'cool', 'huh?']) { |s| s + '!'}      # ["pretty!", "cool!", "huh?!"]
+p multi_map(['pretty', 'cool', 'huh?'], 1) { |s| s + '!'}   # ["pretty!", "cool!", "huh?!"]
+p multi_map(['pretty', 'cool', 'huh?'], 3) { |s| s + '!'}   # ["pretty!!!", "cool!!!", "huh?!!!"]
+p multi_map([4, 3, 2, 7], 1) { |num| num * 10 }             # [40, 30, 20, 70]
+p multi_map([4, 3, 2, 7], 2) { |num| num * 10 }             # [400, 300, 200, 700]
+p multi_map([4, 3, 2, 7], 4) { |num| num * 10 }             # [40000, 30000, 20000, 70000]
+
+def proctition(arr, &prc)
+  true_partition = []
+  false_partition = []
+  arr.each do |ele|
+    if prc.call(ele)
+      true_partition << ele
+    else
+      false_partition << ele
+    end
+  end
+  return true_partition + false_partition
+end
+
+p proctition([4, -5, 7, -10, -2, 1, 3]) { |el| el > 0 }
+# [4, 7, 1, 3, -5, -10, -2]
+
+p proctition([7, 8, 3, 6, 10]) { |el| el.even? }
+# [8, 6, 10, 7, 3]
+
+p proctition(['cat','boot', 'dog', 'bug', 'boat']) { |s| s[0] == 'b' }
+# ["boot", "bug", "boat", "cat", "dog"]
+
+# Phase 3: Perilous
+
+def selected_map!(arr, prc_one, prc_two)
+  arr.each_with_index do |ele, idx|
+    if prc_one.call(ele)
+      arr[idx] = prc_two.call(ele)
+    end
+  end
+  return nil
+end
+
+is_even = Proc.new { |n| n.even? }
+is_positive = Proc.new { |n| n > 0 }
+square = Proc.new { |n| n * n }
+flip_sign = Proc.new { |n| -n }
+
+arr_1 = [8, 5, 10, 4]
+p selected_map!(arr_1, is_even, square)     # nil
+p arr_1                                     # [64, 5, 100, 16]
+
+arr_2 = [-10, 4, 7, 6, -2, -9]
+p selected_map!(arr_2, is_even, flip_sign)  # nil
+p arr_2                                     # [10, -4, 7, -6, 2, -9]
+
+arr_3 = [-10, 4, 7, 6, -2, -9]
+p selected_map!(arr_3, is_positive, square) # nil
+p arr_3                                     # [-10, 16, 49, 36, -2, -9]
+
+def chain_map(val, prcs)
+  prcs.each do |prc|
+    val = prc.call(val)
+  end
+  return val
+end
+
+add_5 = Proc.new { |n| n + 5 }
+half = Proc.new { |n| n / 2.0 }
+square = Proc.new { |n| n * n }
+
+p chain_map(25, [add_5, half])          # 15.0
+p chain_map(25, [half, add_5])          # 17.5
+p chain_map(25, [add_5, half, square])  # 225
+p chain_map(4, [square, half])          # 8
+p chain_map(4, [half, square])          # 4
+
+# require "byebug"
+
+def proc_suffix(sentence, hash)
+  # debugger
+  new_words = []
+  words = sentence.split
+  words.each_with_index do |word, idx|
+    hash.each do |prc, suffix|
+      if prc.call(word)
+        if new_words[idx] == nil
+          new_words << word + suffix
+        else
+          new_words[idx] += suffix
+        end
+      else
+        new_words << word if new_words[idx] == nil
+      end
+    end
+  end
+  return new_words.join(" ")
+end
+
+contains_a = Proc.new { |w| w.include?('a') }
+three_letters = Proc.new { |w| w.length == 3 }
+four_letters = Proc.new { |w| w.length == 4 }
+
+p proc_suffix('dog cat',
+    contains_a => 'ly',
+    three_letters => 'o'
+)   # "dogo catlyo"
+
+p proc_suffix('dog cat',
+    three_letters => 'o',
+    contains_a => 'ly'
+)   # "dogo catoly"
+
+p proc_suffix('wrong glad cat',
+    contains_a => 'ly',
+    three_letters => 'o',
+    four_letters => 'ing'
+)   # "wrong gladlying catlyo"
+
+p proc_suffix('food glad rant dog cat',
+    four_letters => 'ing',
+    contains_a => 'ly',
+    three_letters => 'o'
+)   # "fooding gladingly rantingly dogo catlyo"
+
+def proctition_platinum(arr, *prcs)
+  partition_hash = {}
+
+  prcs.each_with_index { |el, i| partition_hash[i+1] = [] }
+
+  arr.each do |ele|
+    flag = false
+    prcs.each_with_index do |block, idx|
+      if block.call(ele) && !flag
+        partition_hash[idx+1] << ele
+        flag = true
+      end
+    end
+  end
+
+  return partition_hash
+end
+
+is_yelled = Proc.new { |s| s[-1] == '!' }
+is_upcase = Proc.new { |s| s.upcase == s }
+contains_a = Proc.new { |s| s.downcase.include?('a') }
+begins_w = Proc.new { |s| s.downcase[0] == 'w' }
+
+p proctition_platinum(['WHO', 'what', 'when!', 'WHERE!', 'how', 'WHY'], is_yelled, contains_a)
+# {1=>["when!", "WHERE!"], 2=>["what"]}
+
+p proctition_platinum(['WHO', 'what', 'when!', 'WHERE!', 'how', 'WHY'], is_yelled, is_upcase, contains_a)
+# {1=>["when!", "WHERE!"], 2=>["WHO", "WHY"], 3=>["what"]}
+
+p proctition_platinum(['WHO', 'what', 'when!', 'WHERE!', 'how', 'WHY'], is_upcase, is_yelled, contains_a)
+# {1=>["WHO", "WHERE!", "WHY"], 2=>["when!"], 3=>["what"]}
+
+p proctition_platinum(['WHO', 'what', 'when!', 'WHERE!', 'how', 'WHY'], begins_w, is_upcase, is_yelled, contains_a)
+# {1=>["WHO", "what", "when!", "WHERE!", "WHY"], 2=>[], 3=>[], 4=>[]}
+
+def procipher(sentence, hash)
+  new_words = []
+  words = sentence.split
+
+  words.each_with_index do |word, idx|
+    hash.each do |key_prc, val_prc|
+      if key_prc.call(word)
+        if new_words[idx] == nil
+          new_words << val_prc.call(word)
+        else
+          new_words[idx] = val_prc.call(new_words[idx])
+        end
+      else
+        new_words << word if new_words[idx] == nil
+      end
+    end
+  end
+
+  return new_words.join(" ")
+end
+
+is_yelled = Proc.new { |s| s[-1] == '!' }
+is_upcase = Proc.new { |s| s.upcase == s }
+contains_a = Proc.new { |s| s.downcase.include?('a') }
+make_question = Proc.new { |s| s + '???' }
+reverse = Proc.new { |s| s.reverse }
+add_smile = Proc.new { |s| s + ':)' }
+
+p procipher('he said what!',
+    is_yelled => make_question,
+    contains_a => reverse
+) # "he dias ???!tahw"
+
+p procipher('he said what!',
+    contains_a => reverse,
+    is_yelled => make_question
+) # "he dias !tahw???"
+
+p procipher('he said what!',
+    contains_a => reverse,
+    is_yelled => add_smile
+) # "he dias !tahw:)"
+
+p procipher('stop that taxi now',
+    is_upcase => add_smile,
+    is_yelled => reverse,
+    contains_a => make_question
+) # "stop that??? taxi??? now"
+
+p procipher('STOP that taxi now!',
+    is_upcase => add_smile,
+    is_yelled => reverse,
+    contains_a => make_question
+) # "STOP:) that??? taxi??? !won"
+
+def picky_procipher(sentence, hash)
+  new_words = []
+  words = sentence.split
+
+  words.each_with_index do |word, idx|
+    flag = false
+    hash.each do |key_prc, val_prc|
+      if key_prc.call(word) && !flag
+        flag = true
+        if new_words[idx] == nil
+          new_words[idx] = val_prc.call(word)
+        else
+          new_words[idx] = val_prc.call(new_words[idx])
+        end
+      else
+        new_words << word if new_words[idx] == nil
+      end
+    end
+  end
+
+  return new_words.join(" ")
+end
+
+is_yelled = Proc.new { |s| s[-1] == '!' }
+is_upcase = Proc.new { |s| s.upcase == s }
+contains_a = Proc.new { |s| s.downcase.include?('a') }
+make_question = Proc.new { |s| s + '???' }
+reverse = Proc.new { |s| s.reverse }
+add_smile = Proc.new { |s| s + ':)' }
+
+p picky_procipher('he said what!',
+    is_yelled => make_question,
+    contains_a => reverse
+) # "he dias what!???"
+
+p picky_procipher('he said what!',
+    contains_a => reverse,
+    is_yelled => make_question
+) # "he dias !tahw"
+
+p picky_procipher('he said what!',
+    contains_a => reverse,
+    is_yelled => add_smile
+) # "he dias !tahw"
+
+p picky_procipher('stop that taxi now',
+    is_upcase => add_smile,
+    is_yelled => reverse,
+    contains_a => make_question
+) # "stop that??? taxi??? now"
+
+p picky_procipher('STOP that taxi!',
+    is_upcase => add_smile,
+    is_yelled => reverse,
+    contains_a => make_question
+) # "STOP:) that??? !ixat"
