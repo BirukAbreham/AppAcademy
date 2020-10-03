341a185cab933c27248af585eec9547de5211625
BirukAbreham <birukabreham0@gmail.com>
2020-10-03 17:24:46 +0300

    Completed sql zoo exercise as much as I can

---
+++ SQL/SQL_ZOO/sql_zoo_ex/lib/04a_countries_bonus.rb
@@ -0,0 +1,60 @@
+# == Schema Information
+#
+# Table name: countries
+#
+#  name        :string       not null, primary key
+#  continent   :string
+#  area        :integer
+#  population  :integer
+#  gdp         :integer
+
+require_relative './sqlzoo.rb'
+
+def highest_gdp
+  # Which countries have a GDP greater than every country in Europe? (Give the
+  # name only. Some countries may have NULL gdp values)
+  execute(<<-SQL)
+    SELECT
+      name
+    FROM
+      countries
+    WHERE
+      gdp > (
+        SELECT
+          MAX(gdp)
+        FROM
+          countries
+        WHERE
+          continent = 'Europe'
+      )
+  SQL
+end
+
+def largest_in_continent
+  # Find the largest country (by area) in each continent. Show the continent,
+  # name, and area.
+  execute(<<-SQL)
+    SELECT
+      continent,
+      name,
+      area
+    FROM
+      countries
+    WHERE
+      area IN (
+        SELECT
+          MAX(area)
+        FROM
+          countries
+        GROUP BY 
+          continent
+      )
+  SQL
+end
+
+def large_neighbors
+  # Some countries have populations more than three times that of any of their
+  # neighbors (in the same continent). Give the countries and continents.
+  execute(<<-SQL)
+  SQL
+end
