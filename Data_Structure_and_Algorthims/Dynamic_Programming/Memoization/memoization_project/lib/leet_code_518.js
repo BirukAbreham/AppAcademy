4dc3518bd8352eaece6d5802ce28e16c51bd041c
BirukAbreham <birukabreham0@gmail.com>
2021-01-04 15:33:37 +0300

    Data Structure
    Dynamic Programming Memoization - project half completed

---
+++ Data_Structure_and_Algorthims/Dynamic_Programming/Memoization/memoization_project/lib/leet_code_518.js
@@ -0,0 +1,28 @@
+// Work through this problem on https://leetcode.com/problems/coin-change-2/ and use the specs given there.
+// Feel free to use this file for scratch work.
+
+// You are given coins of different denominations and a total amount
+// of money. Write a function to compute the number of combinations
+// that make up that amount. You may assume that you have infinite number
+// of each kind of coin.
+//
+// Example:
+// coinChange([1, 2, 5], 5)  // => 4
+// explanation, there are four ways to make up the amount:
+// 5=5
+// 5=2+2+1
+// 5=2+1+1+1
+// 5=1+1+1+1+1
+//
+// Examples:
+// coinChange([1, 2, 5], 5) // => 4
+// coinChange([2], 3)       // => 0
+// coinChange([10]), 10)    // => 1
+
+function coinChange(coins, amount, memo = {}) {
+  if (amount === 0) return 0;
+}
+
+module.exports = {
+  coinChange,
+};
